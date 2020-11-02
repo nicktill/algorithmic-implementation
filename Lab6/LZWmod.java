a556aed57e78ad73916999b73c95f9425739f4da
Nick Tillmann <57879193+NickTill@users.noreply.github.com>
2020-11-02 13:08:23 -0500

    Add files via upload

---
+++ Lab6/LZWmod.java
@@ -0,0 +1,88 @@
+/*************************************************************************
+ *  Compilation:  javac LZWmod.java
+ *  Execution:    java LZWmod - < input.txt   (compress)
+ *  Execution:    java LZWmod + < input.txt   (expand)
+ *  Dependencies: BinaryStdIn.java BinaryStdOut.java
+ *
+ *  Compress or expand binary input from standard input using LZW.
+ *
+ *
+ *************************************************************************/
+
+public class LZWmod {
+    private static final int R = 256;        // number of input chars
+    private static final int L = 4096;       // number of codewords = 2^W
+    private static final int W = 12;         // codeword width
+
+    public static void compress() {
+      //TODO:Modify TSTmod so that the key is a
+      //StringBuilder instead of String
+        TSTmod<Integer> st = new TSTmod<Integer>();
+        for (int i = 0; i < R; i++)
+            st.put(new StringBuilder("" + (char) i), i);
+        int code = R+1;  // R is codeword for EOF
+
+        //initialize the current string
+        StringBuilder current = new StringBuilder();
+        //read and append the first char
+        char c = BinaryStdIn.readChar();
+        current.append(c);
+        Integer codeword = st.get(current);
+        while (!BinaryStdIn.isEmpty()) {
+            codeword = st.get(current);
+            //TODO: read and append the next char to current
+            char next = BinaryStdIn.readChar();
+            current.append(next);
+            if(!st.contains(current)){
+              BinaryStdOut.write(codeword, W);
+              if (code < L)    // Add to symbol table if not full
+                  st.put(current, code++);
+              //TODO: reset current
+              current = new StringBuilder();
+              current.append(next);
+            }
+        }
+
+        //TODO: Write the codeword of whatever remains
+        //in current
+        codeword = st.get(current);
+        BinaryStdOut.write(codeword, W);
+        BinaryStdOut.write(R, W); //Write EOF
+        BinaryStdOut.close();
+    }
+
+
+    public static void expand() {
+        String[] st = new String[L];
+        int i; // next available codeword value
+
+        // initialize symbol table with all 1-character strings
+        for (i = 0; i < R; i++)
+            st[i] = "" + (char) i;
+        st[i++] = "";                        // (unused) lookahead for EOF
+
+        int codeword = BinaryStdIn.readInt(W);
+        String val = st[codeword];
+
+        while (true) {
+            BinaryStdOut.write(val);
+            codeword = BinaryStdIn.readInt(W);
+            if (codeword == R) break;
+            String s = st[codeword];
+            if (i == codeword) s = val + val.charAt(0);   // special case hack
+            if (i < L) st[i++] = val + s.charAt(0);
+            val = s;
+
+        }
+        BinaryStdOut.close();
+    }
+
+
+
+    public static void main(String[] args) {
+        if      (args[0].equals("-")) compress();
+        else if (args[0].equals("+")) expand();
+        else throw new RuntimeException("Illegal command line argument");
+    }
+
+}
---
+++ Lab6/TSTmod.java
@@ -0,0 +1,180 @@
+/******************************************************************************
+ *  Compilation:  javac TSTmod.java
+ *  Execution:    java TSTmod < words.txt
+ *  Dependencies: StdIn.java
+ *  Data files:   https://algs4.cs.princeton.edu/52trie/shellsST.txt
+ *
+ *  Symbol table with StringBuilder keys, implemented using a ternary search
+ *  trie (TST).
+ *
+ *
+ *  % java TSTmod < shellsST.txt
+ *  keys(""):
+ *  by 4
+ *  sea 6
+ *  sells 1
+ *  she 0
+ *  shells 3
+ *  shore 7
+ *  the 5
+ *
+ *  longestPrefixOf("shellsort"):
+ *  shells
+ *
+ *  keysWithPrefix("shor"):
+ *  shore
+ *
+ *  keysThatMatch(".he.l."):
+ *  shells
+ *
+ *  % java TSTmod
+ *  theory the now is the time for all good men
+ *
+ *  Remarks
+ *  --------
+ *    - can't use a key that is the empty string ""
+ *
+ ******************************************************************************/
+/**
+ *  The {@code TSTmod} class represents an symbol table of key-value
+ *  pairs, with string keys and generic values.
+ *  It supports the usual <em>put</em>, <em>get</em>, <em>contains</em>,
+ *  <em>delete</em>, <em>size</em>, and <em>is-empty</em> methods.
+ *  It also provides character-based methods for finding the string
+ *  in the symbol table that is the <em>longest prefix</em> of a given prefix,
+ *  finding all strings in the symbol table that <em>start with</em> a given prefix,
+ *  and finding all strings in the symbol table that <em>match</em> a given pattern.
+ *  A symbol table implements the <em>associative array</em> abstraction:
+ *  when associating a value with a key that is already in the symbol table,
+ *  the convention is to replace the old value with the new value.
+ *  Unlike {@link java.util.Map}, this class uses the convention that
+ *  values cannot be {@code null}—setting the
+ *  value associated with a key to {@code null} is equivalent to deleting the key
+ *  from the symbol table.
+ *  <p>
+ *  This implementation uses a ternary search trie.
+ *  <p>
+ *  For additional documentation, see <a href="https://algs4.cs.princeton.edu/52trie">Section 5.2</a> of
+ *  <i>Algorithms, 4th Edition</i> by Robert Sedgewick and Kevin Wayne.
+ */
+public class TSTmod<Value> {
+    private int n;              // size
+    private Node<Value> root;   // root of TST
+
+    private static class Node<Value> {
+        private char c;                        // character
+        private Node<Value> left, mid, right;  // left, middle, and right subtries
+        private Value val;                     // value associated with string
+    }
+
+    /**
+     * Initializes an empty string symbol table.
+     */
+    public TSTmod() {
+    }
+
+    /**
+     * Returns the number of key-value pairs in this symbol table.
+     * @return the number of key-value pairs in this symbol table
+     */
+    public int size() {
+        return n;
+    }
+
+    /**
+     * Does this symbol table contain the given key?
+     * @param key the key
+     * @return {@code true} if this symbol table contains {@code key} and
+     *     {@code false} otherwise
+     * @throws IllegalArgumentException if {@code key} is {@code null}
+     */
+    public boolean contains(StringBuilder key) {
+        if (key == null) {
+            throw new IllegalArgumentException("argument to contains() is null");
+        }
+        return get(key) != null;
+    }
+
+    /**
+     * Returns the value associated with the given key.
+     * @param key the key
+     * @return the value associated with the given key if the key is in the symbol table
+     *     and {@code null} if the key is not in the symbol table
+     * @throws IllegalArgumentException if {@code key} is {@code null}
+     */
+    public Value get(StringBuilder key) {
+        if (key == null) {
+            throw new IllegalArgumentException("calls get() with null argument");
+        }
+        if (key.length() == 0) throw new IllegalArgumentException("key must have length >= 1");
+        Node<Value> x = get(root, key, 0);
+        if (x == null) return null;
+        return x.val;
+    }
+
+    // return subtrie corresponding to given key
+    private Node<Value> get(Node<Value> x, StringBuilder key, int d) {
+        if (x == null) return null;
+        if (key.length() == 0) throw new IllegalArgumentException("key must have length >= 1");
+        char c = key.charAt(d);
+        if      (c < x.c)              return get(x.left,  key, d);
+        else if (c > x.c)              return get(x.right, key, d);
+        else if (d < key.length() - 1) return get(x.mid,   key, d+1);
+        else                           return x;
+    }
+
+    /**
+     * Inserts the key-value pair into the symbol table, overwriting the old value
+     * with the new value if the key is already in the symbol table.
+     * If the value is {@code null}, this effectively deletes the key from the symbol table.
+     * @param key the key
+     * @param val the value
+     * @throws IllegalArgumentException if {@code key} is {@code null}
+     */
+    public void put(StringBuilder key, Value val) {
+        if (key == null) {
+            throw new IllegalArgumentException("calls put() with null key");
+        }
+        if (!contains(key)) n++;
+        root = put(root, key, val, 0);
+    }
+
+    private Node<Value> put(Node<Value> x, StringBuilder key, Value val, int d) {
+        char c = key.charAt(d);
+        if (x == null) {
+            x = new Node<Value>();
+            x.c = c;
+        }
+        if      (c < x.c)               x.left  = put(x.left,  key, val, d);
+        else if (c > x.c)               x.right = put(x.right, key, val, d);
+        else if (d < key.length() - 1)  x.mid   = put(x.mid,   key, val, d+1);
+        else                            x.val   = val;
+        return x;
+    }
+
+
+}
+
+/******************************************************************************
+ *  Copyright 2002-2018, Robert Sedgewick and Kevin Wayne.
+ *
+ *  This file is part of algs4.jar, which accompanies the textbook
+ *
+ *      Algorithms, 4th edition by Robert Sedgewick and Kevin Wayne,
+ *      Addison-Wesley Professional, 2011, ISBN 0-321-57351-X.
+ *      http://algs4.cs.princeton.edu
+ *
+ *
+ *  algs4.jar is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  algs4.jar is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with algs4.jar.  If not, see http://www.gnu.org/licenses.
+ ******************************************************************************/
